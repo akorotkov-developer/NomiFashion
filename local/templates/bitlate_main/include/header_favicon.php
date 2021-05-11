d3c9440601937b26f3e8956e3fdfed190f745555
akorotkov <89065267799@mail.ru>
2021-05-11 22:33:22 +0300

    Изменить фавикон сайта

--- local/templates/bitlate_main/include/header_favicon.php
+++ local/templates/bitlate_main/include/header_favicon.php
@@ -1,15 +1,14 @@
 <!--favicon begin-->
-<!--<link rel="apple-touch-icon" sizes="180x180" href="<?/*=SITE_DIR*/?>apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?/*=SITE_DIR*/?>favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?/*=SITE_DIR*/?>favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png">
 
-<link rel="icon" type="image/png" sizes="16x16" href="<?/*=SITE_DIR*/?>favicon-16x16.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?/*=SITE_DIR*/?>favicon-16x16.png">-->
+<link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png">
 
 <link rel="shortcut icon" href="<?= SITE_TEMPLATE_PATH?>/images/new_favicon.png" type="image/png">
 
 <link rel="manifest" href="<?=SITE_DIR?>site.webmanifest">
-<link rel="mask-icon" href="<?=SITE_DIR?>safari-pinned-tab.svg" color="#393839">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
 <!--favicon end-->
