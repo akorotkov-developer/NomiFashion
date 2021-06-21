0e088cba95ca1d8f65668e54c3d2234de15140de
akorotkov <89065267799@mail.ru>
2021-06-21 20:51:37 +0300

    Сделать новинки первыйми по ходу

--- local/templates/bitlate_main/header.php
+++ local/templates/bitlate_main/header.php
@@ -352,14 +352,14 @@ global $USER;
                     <div class="main-product-tabs">
                         <div class="advanced-container-medium">
                             <select class="select-tabs hide-for-large">
-                                <option value="#product-tab-recomend"><?=getMessage('TITLE_TAB_RECOMEND')?></option>
                                 <option value="#product-tab-news"><?=getMessage('TITLE_TAB_NEWS')?></option>
+                                <option value="#product-tab-recomend"><?=getMessage('TITLE_TAB_RECOMEND')?></option>
                                 <option value="#product-tab-hits"><?=getMessage('TITLE_TAB_HITS')?></option>
                                 <option value="#product-tab-discount"><?=getMessage('TITLE_TAB_DISCOUNT')?></option>
                             </select>
                             <ul class="tabs inline-block-container text-center show-for-large" id="main-product-tabs" data-tabs>
-                                <li class="tabs-title inline-block-item float-none is-active"><a href="#product-tab-recomend"><span><?=getMessage('TITLE_TAB_RECOMEND')?></span></a></li>
-                                <li class="tabs-title inline-block-item float-none"><a href="#product-tab-news"><span><?=getMessage('TITLE_TAB_NEWS')?></span></a></li>
+                                <li class="tabs-title inline-block-item float-none is-active"><a href="#product-tab-news"><span><?=getMessage('TITLE_TAB_NEWS')?></span></a></li>
+                                <li class="tabs-title inline-block-item float-none"><a href="#product-tab-recomend"><span><?=getMessage('TITLE_TAB_RECOMEND')?></span></a></li>
                                 <li class="tabs-title inline-block-item float-none"><a href="#product-tab-hits"><span><?=getMessage('TITLE_TAB_HITS')?></span></a></li>
                                 <li class="tabs-title inline-block-item float-none"><a href="#product-tab-discount"><span><?=getMessage('TITLE_TAB_DISCOUNT')?></span></a></li>
                             </ul>
@@ -367,7 +367,7 @@ global $USER;
                         <div class="container row tabs-content" data-tabs-content="main-product-tabs">
                             <?$arTabs = array('recomend', 'news', 'hits', 'discount');
                             foreach ($arTabs as $type):?>
-                                <div class="tabs-panel<?if ($type == 'recomend'):?> is-active<?endif;?>" id="product-tab-<?=$type?>">
+                                <div class="tabs-panel<?if ($type == 'news'):?> is-active<?endif;?>" id="product-tab-<?=$type?>">
                                     <div class="products-flex-grid product-grid product-grid-<?=$type?>">
                                         <?$APPLICATION->IncludeFile(
                                             SITE_DIR . "include/popup/product_tab.php",
