6b09dde2ec8eff3dde6fa6fab40d56331644c47c
akorotkov <89065267799@mail.ru>
2021-06-21 22:26:22 +0300

    Косметические правки

--- shop/delivery/index.php
+++ shop/delivery/index.php
@@ -1,170 +1,126 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Доставка");?><h2>Способы доставки</h2>
-<p>
- <b>Какой вариант доставки выбрать? Стоимость доставки – бесплатно</b>
-</p>
-<p>
-	 Москва
-</p>
-<p>
- <b>1. Самовывоз с производства N.O.M.I</b>
-</p>
-<p>
-	 М. Тушинская, МЦД Трикотажная, Волоколамское шоссе, 89
-</p>
-<p>
-	 По рабочим дням с 10 до 19 часов или в выходные по предварительной договоренности. Согласовывать приезд обязательно, для прохождения на территорию необходим пропуск, и выгодно – мы закажем для вас такси – из удобной точки прямо до производства. Свяжитесь, пожалуйста, с нами предварительно +7 495 134 34 37 (только звонки), чтобы согласовать время. Самовывоз с производства следует выбирать, если вам интересно заглянуть за кулисы – мы обязательно проведем для вас экскурсию. Также вы можете заказать здесь индивидуальный пошив, выбрать ткань и фурнитуру.
-</p>
-<p>
- <b>2. Самовывоз из брендового магазина N.O.M.I в ТЦ «Европейский»</b>
-</p>
-<p>
-	 м.Киевская, Этаж 2-Атриум "Рим", телефон в магазине +7 925 554 68 14 (звонки, смс, WhatsApp, Telegram). График работы: понедельник, вторник, среда, четверг, воскресенье — с 10:00 до 22:00. Пятница и суббота — с 10:00 до 23:00.
-</p>
-<p>
-	 Если вы выбрали что-то на сайте, предварительно уточните наличие модели и размера в магазине по телефону. В случае отсутствия мы осуществим доставку к вашему посещению. Выбирайте этот вариант доставки, если вы не располагаете временем, но хотите примерить наши изделия – мы доставим любое изделие с сайта, даже из несезонных коллекций. Зимнее пальто летом? Пожалуйста, все модели и размеры привезем для вас. Время – по предварительному согласованию.&nbsp;
-</p>
-<p>
- <b>Курьерская доставка с примеркой</b>
-</p>
-<p>
-	 Срок доставки 1-2 рабочих дня, доставка в выходные дни возможна по предварительному согласованию. Количество изделий на доставку с примеркой 12 шт., выбирайте модели и размеры. Жаль, что вы не можете посетить нас, но такая доставка поможет посмотреть наши изделия вживую, примерить и выбрать.
-</p>
-<p>
-	 Доставка возможна только по Москве, мы работаем над расширение нашей географии. Примерить наши изделия вы уже можете в Санкт-Петербурге. Уточните у нас наличие понравившегося изделия, в случае отсутствия мы доставим размеры и модели на выбор к нашему представителю, далее доставка и примерка осуществляются по условиям нашего представителя.&nbsp;
-</p>
-<p>
- <b>Другие города</b>
-</p>
- Курьерская доставка без примерки осуществляется в любой город Российской Федерации. Стоимость доставки по России – бесплатно и при полной предоплате заказа. Мы отправим ваш заказ на следующий рабочий день после оформления на сайте, срок доставки зависит от выбранной почтовой службы. Список служб постоянно обновляется и будет предложен нашим менеджером при оформлении заказа с учетом ваших пожеланий.<br>
-<ul class="inner-content-list row large-up-2" style="display:none;">
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-russianpost.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 Почта России
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">10-15 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 350 рублей</span>
-		</div>
-	</div>
- </li>
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-ems.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 EMS
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">5-10 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 500 рублей</span>
-		</div>
-	</div>
- </li>
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-spsr.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 СПСР-Экспресс
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">3-15 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 750 рублей</span>
-		</div>
-		<div class="param">
-			 Дополнительно: <span class="add">Возможность примерки</span>
-		</div>
-	</div>
- </li>
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-dhl.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 DHL
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">2-5 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 1500 рублей</span>
-		</div>
-		<div class="param">
-			 Дополнительно: <span class="add">Бесплатный возврат, частичный выкуп</span>
-		</div>
-	</div>
- </li>
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-ups.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 UPS
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">3-15 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 1750 рублей</span>
-		</div>
-	</div>
- </li>
-	<li class="inner-content-delivery columns table-container">
-	<div class="preview table-item vertical-top">
- <img src="/local/templates/bitlate_main/images/delivery-pony-express.jpg" alt="">
-	</div>
-	<div class="table-item">
-		<div class="param name">
-			 Pony Express
-		</div>
-		<div class="param">
-			 Срок доставки: <span class="warning">10-15 дней</span>
-		</div>
-		<div class="param">
-			 Стоимость: <span class="warning">от 950 рублей</span>
-		</div>
-		<div class="param">
-			 Дополнительно: <span class="add">Доставка габартиных грузов</span>
-		</div>
-	</div>
- </li>
-</ul>
-<div style="display:none;">
-	<h2>Самовывоз</h2>
-	<p>
-		 Вы также можете забрать посылку в одном из наших пунктов выдачи
-	</p>
-	 <?$APPLICATION->IncludeComponent(
-	"bitrix:catalog.store",
-	"",
-	Array(
-		"CACHE_TIME" => "3600",
-		"CACHE_TYPE" => "A",
-		"COMPONENT_TEMPLATE" => ".default",
-		"MAP_TYPE" => "0",
-		"PHONE" => "Y",
-		"SCHEDULE" => "Y",
-		"SEF_FOLDER" => "/company/shops/",
-		"SEF_MODE" => "Y",
-		"SEF_URL_TEMPLATES" => Array("element"=>"#store_id#.html","liststores"=>"index.php"),
-		"SET_TITLE" => "N",
-		"TITLE" => "Список складов с подробной информацией"
-	)
-);?>
+$APPLICATION->SetPageProperty("tags", "бесплатная доставка, быстрая доставка, курьерская доставка с примеркой, доставка и примерка, купить с доставкой, интернет доставка, доставка женской одежды, доставка по России, доставка по РФ, доставка по стране, интернет доставка бесплатно, доставка одежды, интернет магазин с бесплатной доставкой, женская одежда с бесплатной доставкой, интернет доставка по России");
+$APPLICATION->SetPageProperty("description", "Бесплатная доставка товара при совершении покупки на сайте nomi.fashion");
+$APPLICATION->SetTitle("Доставка");?><div><h2>Информация о способах доставки и оплаты</h2></div>
+<div class="inner-content columns" style="float: left; padding-top:20px;">
+                            <ul class="accordion" data-accordion data-allow-all-closed="true" style="background-color:#FFF">
+                    <li class="accordion-item">
+                <a href="#" class="accordion-title inline-block-container" style="background-color:#FFF">
+                    <span class="arrow inline-block-item vertical-middle"style="border-left:#666; border-bottom:#666;"></span>
+                    <span class="desc row inline-block-item vertical-middle">
+                        <strong class="name column large-6" style="color:#666; width:100%">Москва и МО курьером</strong>
+                                                 
+               </span> </a>
+               <div class="accordion-content row" data-tab-content style="border:none; margin-top:-35px">
+                    <div class="inner-content-vacancy-info columns large-8 xxlarge-9" style="width:100%" "color:#666;">
+                        <b style="color:#666"><u>При оплате товара на сайте доставка бесплатная </u></b><br>
+  <ul>
+  <li>Срок доставки 1-2 рабочих дня.</li>
+  <li>Доставка возможна на следующий рабочий день, при оформлении заказа до 15:00, через 1 рабочий день при оформлении заказа после 15:00.</li>
+<li>Доставка осуществляется с 09:00 до 22:00 (ежедневно). </li>
+<li>Курьер свяжется с вами за час до визита.</li>
+<li>Время примерки - 15 минут.</li>
+<li>Оплата при получении картой или наличными. Предоплата 100% картой на сайте</li>
+</ul><br />
+
+ <b style="color:#666"><u>Доставка с примеркой.</u></b>
+<br />
+	В случае выкупа хотя бы 1 позиции заказа доставка бесплатная.<br>
+На примерку можно заказать до 6 изделий. Верхняя одежда до 4 изделий.<br> 
+Оплата заказа наличными или банковской картой курьеру.<br>
+При полном отказе доставка оплачивается в размере 300 р. 
+
 </div>
- <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+                    </div>
+                    
+            </li>
+                   <li class="accordion-item" data-accordion-item id="bx_3218110189_217">
+                <a href="#" class="accordion-title inline-block-container" style="background-color:#FFF">
+                    <span class="arrow inline-block-item vertical-middle" style="border-left:#666; border-bottom:#666;"></span>
+                    <span class="desc row inline-block-item vertical-middle">
+                        <strong class="name column large-6" style="color:#666; width:100%">Москва самовывоз из шоурума Волоколамское шоссе д.89</strong>
+                                            </span>
+                </a>
+                <div class="accordion-content row" data-tab-content style="border:none; margin-top:-35px">
+                    <div class="inner-content-vacancy-info columns large-8 xxlarge-9" style="width:100%" "color:#666;">
+                        
+                        <ul>
+                       <li>С понедельника по пятницу с 09:00 до 19:00</li>
+<li>Обязательно предварительно согласовывается день и время приезда</li>
+<li>Оплата заказа наличными или банковской картой, предоплата на сайте</li>
+</ul>
+
+                    </div>
+                  </div>
+            </li>
+                    <li class="accordion-item" data-accordion-item id="bx_3218110189_217">
+                <a href="#" class="accordion-title inline-block-container" style="background-color:#FFF">
+                    <span class="arrow inline-block-item vertical-middle" style="border-left:#666; border-bottom:#666;"></span>
+                    <span class="desc row inline-block-item vertical-middle">
+                        <strong class="name column large-6" style="color:#666; width:100%">Москва самовывоз из Магазина ТЦ Европейский</strong>
+                                            </span>
+                </a>
+                <div class="accordion-content row" data-tab-content style="border:none; margin-top:-35px">
+                    <div class="inner-content-vacancy-info columns large-8 xxlarge-9" style="width:100%" "color:#666;">
+                        
+                        <ul>
+                        <li>Заказы, оплаченные на сайте, могут быть доставлены для выдачи в ТЦ Европейский, магазин N.O.M.I </li>
+<li>Так же, доступен резерв товара на 1-2 дня без предоплаты.</li>
+</ul>
+
+</div>
+                    </div>
+               
+            </li>
+            <li class="accordion-item" data-accordion-item id="bx_3218110189_217">
+                <a href="#" class="accordion-title inline-block-container" style="background-color:#FFF">
+                    <span class="arrow inline-block-item vertical-middle" style="border-left:#666; border-bottom:#666;"></span>
+                    <span class="desc row inline-block-item vertical-middle">
+                        <strong class="name column large-6" style="color:#666; width:100%">Россия курьером и ПВЗ</strong></span>
+                                         
+                </a>
+                <div class="accordion-content row" data-tab-content style="border:none; margin-top:-35px">
+                    <div class="inner-content-vacancy-info columns large-8 xxlarge-9" style="width:100%" "color:#666;">
+                       
+                        <ul>
+                        <li>Сроки доставки в Ваш город уточняются при размещении заказа. </li>
+</ul>
+<br>
+<b style="color:#666"><u>При оплате на сайте:</u></b><br>
+<ul>
+                        <li>Бесплатная доставка пункты выдачи СДЕК</li>
+                        <li>Бесплатная курьерская доставка</li>
+                        </ul><br />
+<b style="color:#666"><u>Оплата при получении, с примеркой (оплата картой либо наличными при получении)</u></b><br>
+В случае выкупа хотя бы 1 позиции заказа доставка бесплатная.<br>
+На примерку можно заказать до 6 изделий. Верхняя одежда до 4 изделий. <br>
+При полном отказе доставка оплачивается в размере 490 р. <br>
+
+
+</div>
+                    </div>
+               
+            </li>
+            <li class="accordion-item" data-accordion-item id="bx_3218110189_217">
+                <a href="#" class="accordion-title inline-block-container" style="background-color:#FFF">
+                    <span class="arrow inline-block-item vertical-middle" style="border-left:#666; border-bottom:#666;"></span>
+                    <span class="desc row inline-block-item vertical-middle">
+                        <strong class="name column large-6" style="color:#666; width:100%">Доставка в другие страны</strong>
+                                            </span>
+                </a>
+                <div class="accordion-content row" data-tab-content style="border:none; margin-top:-35px">
+                    <div class="inner-content-vacancy-info columns large-8 xxlarge-9" style="width:100%" "color:#666;">
+                        
+                        <ul>
+                        <li>Доставка EMS Почтой России</li>
+<li>Срок доставки: в среднем 7-10 рабочих дней</li>
+</ul>
+
+</div>
+                    </div>
+               
+            </li>
+            </ul>
+            
+                                                    </div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
